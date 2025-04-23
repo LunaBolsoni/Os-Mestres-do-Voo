84583b97f39f35b903de715dbba13b1112a33311
vinyzada <vinyescolas@gmail.com>
2025-04-23 16:37:57 -0300

    feat: criado menu e pagina inicial (HomeView)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { MenuComp } from '@/components';
 </script>
 
 <template>
   <RouterView />
+  <MenuComp />
 </template>
 
 <style scoped>
---
+++ src/components/MenuComp.vue
@@ -0,0 +1,42 @@
+<script setup>
+</script>
+
+<template>
+    <div class="menu">
+        <div class="icon active">
+            <img src="/src/components/icons/house.svg" alt="">
+        </div>
+        <div class="icon">
+            <img src="/src/components/icons/dragon.svg" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.menu{
+    position: absolute;
+    bottom: 80px;
+    left: calc(50% - 100px);
+    width: 200px;
+    display: flex;
+    background-color: #5E58FF;
+    border-radius: 25px;
+    height: 80px;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+}
+.icon{
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    cursor: pointer;
+}
+.active{
+    background-color: #9692FF;
+}
+</style>
