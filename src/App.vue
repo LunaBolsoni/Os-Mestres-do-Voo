798a8758a78518b40c1b145819659ba7bf39d7ce
santosjuliana23 <a.pkskj@gmail.com>
2025-04-28 17:43:27 -0300

    feat: estilização do menu

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,38 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
   <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/dragoes">Dragões</router-link>
+    <div class="menu">
+    <nav class="menu-itens">
+      <router-link to="/"><img src="@/assets/icons/casa2.png" alt="Home" class="icone-casa" /></router-link>
+      <router-link to="/dragoes"><img src="@/assets/icons/Vector.png" alt="Dragoes" class="icone-dragao"></router-link>
     </nav>
   </div>
+</div>
 </template>
 
 <style scoped>
+.menu{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom:60px;
+  left: 850px;
+
+  background-color:#5E58FF;
+  border-radius: 25px;
+  width: 200px;
+  padding: 20px;
+}
+.icone-casa{
+  width: 40px;
+  height: auto;
+  margin-right: 20px;
+}
+.icone-dragao{
+  width: 40px;
+  height: auto;
+  margin-left: 20px;
+}
+
 </style>
