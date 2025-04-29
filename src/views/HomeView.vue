940acfdac03bfbd19c1a9d0b4637ffb2798d7f26
santosjuliana23 <a.pkskj@gmail.com>
2025-04-29 09:17:57 -0300

    feat: botão funcional.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,9 @@
 <script setup>
+import { ref } from 'vue';
+
+function button(){
+
+};
 </script>
 
 <template>
@@ -8,7 +13,9 @@
       <p>O universo de <strong> Como Treinar o Seu Dragão </strong>  se passa em um mundo fictício onde <strong>vikings</strong> convivem com <strong>dragões</strong>. Inicialmente vistos como inimigos, os dragões passam a ser aliados após Soluço, um jovem viking, provar que podem viver em harmonia. A história se desenvolve ao longo de filmes, séries e livros, mostrando a evolução dessa convivência, a criação do <strong>vilarejo Berk</strong> como um refúgio para dragões e os desafios enfrentados para proteger esse novo modo de vida. É um universo rico em aventura, crescimento pessoal e amizade entre humanos e criaturas fantásticas.</p>
     </div>
     <div class="bottom-div">
+      <router-link class="icon" to="/dragons"> 
       <button class="button">Ver Dragões</button>
+      </router-link>
     </div>
   </main>
 </template>
@@ -36,7 +43,13 @@ p{
   border: none;
   background-color: #5E58FF;
   padding: 15px 60px;
+  cursor: pointer;
+  transition: 0.5s all;
 }
+.button:hover{
+  scale: 1.05;
+}
+
 .bottom-div{
   padding-top: 50px;
 }
